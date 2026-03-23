ea43582577818aff9b2d4aa25469d505f4ddcefa
Sneiden <sneiden.dev@gmail.com>
2026-03-23 13:46:42 -0700

    chore(tests): add test setup and sample XML fixture

---
+++ tests/test_parser.py
@@ -0,0 +1,49 @@
+import unittest
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "src"))
+
+from modules.parser import parse_nmap_output
+
+
+SAMPLE_XML = """<?xml version="1.0" encoding="UTF-8"?>
+<nmaprun>
+    <host>
+        <status state="up" reason="localhost-response" reason_ttl="0" />
+        <address addr="192.168.1.33" addrtype="ipv4" />
+        <hostnames>
+            <hostname name="Admin" type="PTR" />
+        </hostnames>
+    </host>
+    <host>
+        <status state="up" reason="arp-response" reason_ttl="0" />
+        <address addr="192.168.1.1" addrtype="ipv4" />
+        <address addr="AA:BB:CC:DD:EE:FF" addrtype="mac" vendor="Cisco Systems" />
+        <hostnames>
+            <hostname name="router.local" type="PTR" />
+        </hostnames>
+    </host>
+    <host>
+        <status state="up" reason="arp-response" reason_ttl="0" />
+        <address addr="192.168.1.20" addrtype="ipv4" />
+        <address addr="FF:EE:DD:CC:BB:AA" addrtype="mac" vendor="Test vendor" />
+        <hostnames>
+            <hostname name="Test name" type="PTR" />
+        </hostnames>
+    </host>
+    <host>
+        <status state="up" reason="arp-response" reason_ttl="0" />
+        <hostnames>
+        </hostnames>
+    </host>
+</nmaprun>"""
+
+
+class TestParser(unittest.TestCase):
+    def setUp(self):
+        self.devices = parse_nmap_output(SAMPLE_XML)
+
+
+if __name__ == "__main__":
+    unittest.main()
