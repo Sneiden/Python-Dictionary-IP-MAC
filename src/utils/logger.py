789f23435e4eb538cba64654d03a8685697e61a1
Sneiden <sneiden.dev@gmail.com>
2026-03-23 12:05:52 -0700

    feat(logger): apply formatter to file and console handlers

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -41,3 +41,14 @@ def setup_logger(name: str = "ip_mac_scanner") -> logging.Logger:
         file_handler.setLevel(logging.DEBUG)
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            "[%(asctime)s] %(levelname)s - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S"
+        )
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+    return logger
